6bb05a48b829925b0af2f91a4b3dc9b87118095e
Emil Öhlund <emil@emilohlund.dev>
2024-03-03 18:36:39 +0100

    feat:

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === "production" ? "/metaroute" : "",
-  output: "export",
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
