23b48030882816324b69806e33d4390ef6582c54
Emil Öhlund <emil@emilohlund.dev>
2024-03-04 03:11:28 +0100

    fix:

--- docs/app/components/DocsCodeSnippet.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from "react";
-
-type Props = {
-  snippet: string;
-};
-
-export default function DocsCodeSnippet({ snippet }: Props) {
-  return <code className="bg-gray-100 p-1 rounded-md">{snippet}</code>;
-}
--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -4,31 +4,39 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center">
-        <img
-          className="select-none"
-          src="/metaroute-logo.png"
-          alt="Next.js Logo"
-          width={360}
-        />
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(splash-art.png)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-10"></div>
+      <div className="hero-content text-center text-neutral-content -mt-20">
+        <div className="max-w-md">
+          <img
+            className="select-none"
+            src="/metaroute-logo.png"
+            alt="Next.js Logo"
+            width={360}
+          />
 
-        <div className="flex gap-x-4">
-          <Link href="docs">
-            <button className="btn btn-outline btn-secondary">
-              Documentation
-            </button>
-          </Link>
-          <Link
-            href="https://github.com/emilohlund-git/metaroute"
-            target="_blank"
-          >
-            <button className="btn btn-outline btn-accent">
-              <FaGithub className="text-xl" />
-            </button>
-          </Link>
+          <div className="flex gap-x-4 justify-center">
+            <Link href="docs">
+              <button className="btn btn-outline btn-success">
+                Documentation
+              </button>
+            </Link>
+            <Link
+              href="https://github.com/emilohlund-git/metaroute"
+              target="_blank"
+            >
+              <button className="btn btn-outline btn-info">
+                <FaGithub className="text-xl" />
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
