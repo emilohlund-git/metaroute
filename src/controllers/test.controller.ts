f25c17cf0185aacc04b35a39e646f9f93776a59d
Emil Öhlund <emil@emilohlund.dev>
2024-03-08 14:11:55 +0100

    fix:

--- src/controllers/test.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, ResponseEntity } from "@core/api";
-
-@Controller("/test")
-export class TestController {
-  @Get("/")
-  public async get() {
-    return ResponseEntity.ok();
-  }
-}
--- src/test.ts
+++
@@ -1,6 +0,0 @@
-import { App, Application, JsonMiddleware } from "./core";
-
-@App({
-  middleware: [JsonMiddleware],
-})
-export class MetaApp extends Application {}
